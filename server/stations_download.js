cd23109db46309c962896f822e85770c30693a62
HunterNL <hunternl@users.noreply.github.com>
2017-07-01 20:02:24 +0200

    cache station list

--- server/stations_download.js
+++ server/stations_download.js
@@ -3,8 +3,11 @@ import { getStationList } from '../imports/ns';
 const stations = getStationList();
 const PATH = Meteor.settings.public.STATION_DOWNLOAD_PATH;
 
+const DAY = 60 * 60 * 24;
+
 WebApp.connectHandlers.use(PATH, (req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-control', 'public, max-age=' + DAY); // eslint-disable-line prefer-template
   res.writeHead(200);
   res.end(JSON.stringify(stations));
 });
